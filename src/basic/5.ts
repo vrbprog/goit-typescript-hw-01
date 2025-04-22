a6b1ec6f5330c349d29df3d11438a3b94075c4d2
Roman <vrbox@i.ua>
2025-04-22 12:32:23 +0300

    Update

--- src/basic/5.ts
+++ src/basic/5.ts
@@ -1,11 +1,11 @@
 enum DayOfWeek {
-  Monday,
-  Tuesday,
-  Wednesday,
-  Thursday,
-  Friday,
-  Saturday,
-  Sunday
+ Monday,
+ Tuesday,
+ Wednesday,
+ Thursday,
+ Friday,
+ Saturday,
+ Sunday
 }
 
 

07297c15c1a877fe4658972cb4b2857a35ea4234
Roman <vrbox@i.ua>
2025-04-22 12:31:03 +0300

    Add Basic

---
+++ src/basic/2.ts
@@ -0,0 +1,4 @@
+
+let person: [string, number] = ['Max', 21];
+
+console.log(person);
---
+++ src/basic/3.ts
@@ -0,0 +1,5 @@
+
+let stnum: string | number = 'Max';
+let lit: "enable" | "disable" = "enable";
+
+console.log(stnum, lit);
